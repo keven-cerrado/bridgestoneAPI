8d978aebc6e6e286e2a2f86dfc400afd393c4666
keven-cerrado <keven.barbosa@cerradopneus.com.br>
2024-08-12 12:01:22 -0300

    Atualizar tratamento de erro ao iniciar agendamento no evento de startup

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ from .database import SessionLocal
 
 app = FastAPI()
 
-print("Iniciando aplicação...")
-
 
 # Dependency
 def get_db():
@@ -22,18 +20,16 @@ def get_db():
 # app.include_router(login.router)
 app.include_router(faturamento.router)
 
-print("Iniciando aplicação...")
-
 
 # Função para ser chamada no evento de startup
 def iniciar_agendamento_thread():
+    print("Iniciando agendamento...")
     try:
         iniciar_agendamento()
     except Exception as e:
         print(f"Erro ao iniciar agendamento: {e}")
 
 
-print("Iniciando aplicação...")
 # Adicionar o evento de startup
 app.add_event_handler(
     "startup", lambda: threading.Thread(target=iniciar_agendamento_thread).start()
